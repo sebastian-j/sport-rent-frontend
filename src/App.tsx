1da44680f60d1fb40c863ae09a883a7b777230f0
Dawid Szłapa <57680129+Dawudd@users.noreply.github.com>
2026-07-07 14:29:40 +0200

    feat/panoramic-image (#10)
    * refactor: add ButtonCore
    
    * feat: add panoramic image
    
    * fix: formatting
    
    * fix:
    
    * fix: formatting
    
    ---------
    
    Co-authored-by: Szymon Balicki <149063121+Simi1354@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import ProductCard from './components/ProductCard.tsx';
+import bikeImage from './assets/bike.png';
+import panoramicImage from './assets/panoramic.png';
+import * as React from 'react';
+import PanoramicImage from './components/PanoramicImage.tsx';
 import Footer from './components/Footer';
 
 function App() {
+  const [isFavorite, setIsFavorite] = React.useState(false);
+
+  const onFavoriteToggle = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
-    <>
+    <div className="flex flex-col">
+      <div className="w-full">
+        <PanoramicImage image={panoramicImage} title="Deski SUP" />
+      </div>
+      <ProductCard
+        name="Rower szosowy"
+        price={89}
+        image={bikeImage}
+        alt="Zdjęcie rowera"
+        onClick={() => alert('Dodano!')}
+        isFavorite={isFavorite}
+        onFavoriteToggle={onFavoriteToggle}
+      />
       <Footer />
-    </>
+    </div>
   );
 }
 
