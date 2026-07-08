1f9d44a014c6f898d90576d85a68511200edd290
Szymon Balicki <149063121+Simi1354@users.noreply.github.com>
2026-07-08 14:42:35 +0200

    Feat/login page (#26)
    * feat: add login and register pages, add protected routes
    
    * fix: formatting
    
    * fix: remove register button from login page
    
    * fix: remove dead code
    
    * fix: switched to useState

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import ContactPage from './pages/ContactPage.tsx';
 import PointsPage from './pages/PointsPage.tsx';
 import TosPage from './pages/TosPage.tsx';
 import FaqPage from './pages/FaqPage.tsx';
+import LoginLayout from './layouts/LoginLayout.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import RegisterPage from './pages/RegisterPage.tsx';
+import ProtectedRoute from './components/auth/ProtectedRoute.tsx';
 
 function App() {
   return (
@@ -17,16 +21,21 @@ function App() {
         <Route element={<Layout />}>
           {/*Header*/}
           <Route path="/" element={<HomePage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-
+          <Route element={<ProtectedRoute />}>
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
           {/*Footer*/}
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/points" element={<PointsPage />} />
           <Route path="/tos" element={<TosPage />} />
           <Route path="/faq" element={<FaqPage />} />
+        </Route>
+        <Route element={<LoginLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
