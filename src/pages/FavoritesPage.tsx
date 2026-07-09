8212d18329301ee855ec95d829a1aeb3bed5b90c
Simi1354 <sbalicki@student.agh.edu.pl>
2026-07-09 13:31:30 +0200

    fix: importing

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ProductCard from '../components/ProductCard';
+import ProductCard from '../components/product/ProductCard';
 import bikeImage from '../assets/bike.png';
 
 const INITIAL_FAVORITES = [
