c51d9404bd2b2d07cc908b5e05eed339d0f83002
dszlapa <dawidszlapaz@gmail.com>
2026-07-10 13:36:30 +0200

    fix: points calculation

--- src/features/profile/LoyaltySection.tsx
+++ src/features/profile/LoyaltySection.tsx
@@ -2,11 +2,13 @@ import PointsCard from './loyalty/PointsCard.tsx';
 import { POINTS_ACQUISITIONS } from './loyalty/pointsAcquisitions.ts';
 
 export default function LoyaltySection() {
+  const pointsSum = POINTS_ACQUISITIONS.reduce((sum, acquisition) => sum + acquisition.amount, 0);
+
   return (
     <section className="flex w-full flex-col items-center justify-center pt-12 text-app-text">
       <p className="text-center text-5xl">Program lojalnościowy</p>
       <p className="mt-4 text-3xl">
-        Posiadasz <span className="font-semibold">1567</span> punktów
+        Posiadasz <span className="font-semibold">{pointsSum}</span> punktów
       </p>
       <div className="m-12 flex w-full max-w-[calc(100%-6rem)] flex-col gap-0.5 overflow-hidden rounded-xl bg-app-surfaceSoft">
         {POINTS_ACQUISITIONS.map((acquisition) => (
