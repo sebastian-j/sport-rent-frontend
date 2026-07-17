b2a343b4482b3580809596ee1fd0d0f57a2a7fe0
dszlapa <dawidszlapaz@gmail.com>
2026-07-17 11:42:15 +0200

    fix: add ButtonCore min dimensions

--- src/components/core/componentStyles.ts
+++ src/components/core/componentStyles.ts
@@ -8,7 +8,7 @@ export const componentStyles = {
   },
 
   button: {
-    base: 'rounded-lg',
+    base: 'rounded-lg min-w-24 min-h-8',
     tone: {
       default: 'bg-app-surfaceStrong text-app-textInverted',
       inversed: 'bg-app-surface text-app-text',
