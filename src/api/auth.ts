74b3130d75cb70b4db42ecaf4fa92628e07a409f
dszlapa <dawidszlapaz@gmail.com>
2026-07-21 13:54:51 +0200

    feat: use paths

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,9 @@
-import type { components } from './generated/schema.ts';
+import type { paths } from './generated/schema.ts';
 import { request } from './client.ts';
 
-type LoginRequest = components['schemas']['LoginRequest'];
-type LoginResponse = components['schemas']['LoginResponse'];
+type LoginOperation = paths['/auth/login']['post'];
+type LoginRequest = LoginOperation['requestBody']['content']['application/json'];
+type LoginResponse = LoginOperation['responses'][200]['content']['application/json'];
 
 export const login = (body: LoginRequest): Promise<LoginResponse | undefined> =>
-  request('auth/login', { method: 'POST', body });
+  request<LoginResponse>('/auth/login', { method: 'POST', body });
