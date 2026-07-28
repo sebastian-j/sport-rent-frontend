dc762618ba0340553915ed830a66957e3b2f6a70
dszlapa <dawidszlapaz@gmail.com>
2026-07-28 10:25:49 +0200

    feat: add register

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,8 +4,10 @@ import type { components } from './generated/schema.ts';
 
 type LoginRequest = components['schemas']['LoginRequest'];
 type ResetPasswordRequest = components['schemas']['ResetPasswordRequest'];
+type RegisterRequest = components['schemas']['RegisterRequest'];
 
 export const login = (body: LoginRequest) => api.POST('/auth/login', { body });
 export const logout = () => api.POST('/auth/logout', { headers: getCsrfHeaders() });
 export const resetPassword = (body: ResetPasswordRequest) =>
   api.POST('/auth/reset-password', { body });
+export const register = (body: RegisterRequest) => api.POST('/auth/register', { body });
